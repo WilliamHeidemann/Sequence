69bbe1ec1b47fe58b37ee34c19e6a7cc4b2e8e70
William Skou Heidemann <Williamskou2227@gmail.com>
2026-09-23 09:34:05 +0200

    Set up memory test

--- Cloud_Code_Module_Reference/Project/PushMessagesService.cs
+++ Cloud_Code_Module_Reference/Project/PushMessagesService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Unity.Services.CloudCode.Core;
 using Unity.Services.CloudCodePush.Model;
@@ -7,12 +8,31 @@ namespace Cloud_Code_Module_Reference;
 public class PushMessagesService(IPushClient pushClient)
 {
     [CloudCodeFunction("ChallengeFriend")]
-    public async Task<string> ChallengeFriend(IExecutionContext context, 
-        string message, string messageType, string playerId)
+    public async Task<string> ChallengeFriend(IExecutionContext context,
+        string challengerName, string challengedPlayerId)
     {
-        SendMessageReply response = 
-            await pushClient.SendPlayerMessageAsync(context, message, messageType, playerId);
+        const string messageType = "GameRequest";
+        
+        SendMessageReply response =
+            await pushClient.SendPlayerMessageAsync(context, challengerName, messageType, challengedPlayerId);
 
         return "Message sent";
+    }
+
+    public async Task<bool> NotifyOpponentOfMove(IExecutionContext context, string matchId, string opponentPlayerId)
+    {
+        try
+        {
+            const string messageType = "MoveNotification";
+            
+            SendMessageReply response =
+                await pushClient.SendPlayerMessageAsync(context, message, messageType, opponentPlayerId);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
     }
 }
